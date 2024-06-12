39b2d0c6e7670922b7e9f7077123c240fe585dba
VitaliiPodshibyakin <vitalikpod@yandex.ru>
2024-06-12 22:42:15 +0300

    Initial Commit

---
+++ LogInApp/LogInVC.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LogInApp
+//
+//  Created by Виталий Подшибякин on 12.06.2024.
+//
+
+import UIKit
+
+class LogInVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
